753869df79ee85597d2cc8ddbf98105eaa07c13d
Fabrice Servais <fabrice.servais@gmail.com>
2018-05-07 01:47:49 +0200

    Completing coverage for api.js

--- test/test.js
+++ test/test.js
@@ -16,6 +16,42 @@ describe('common/API', function() {
                 'body': JSON.stringify({"key": "value"})
             });
         });
+
+        it('should output a (AWS) correctly formatted response for a HTTP 400', function () {
+            let resp = _api.build_response(400, {"key": "value"});
+
+            assert.typeOf(resp, 'Object');
+            expect(resp).to.deep.equal({
+                'statusCode': 400,
+                'headers': {"Content-Type": "application/json"},
+                'isBase64Encoded': false,
+                'body': JSON.stringify({"key": "value"})
+            });
+        });
+
+        it('should output a (AWS) correctly formatted response without body', function () {
+            let resp = _api.build_response(400);
+
+            assert.typeOf(resp, 'Object');
+            expect(resp).to.deep.equal({
+                'statusCode': 400,
+                'headers': {"Content-Type": "application/json"},
+                'isBase64Encoded': false
+            });
+        });
+
+
+        it('should output a (AWS) correctly formatted response with isBase64Encoded', function () {
+            let resp = _api.build_response(400, {"key": "value"}, true);
+
+            assert.typeOf(resp, 'Object');
+            expect(resp).to.deep.equal({
+                'statusCode': 400,
+                'headers': {"Content-Type": "application/json"},
+                'isBase64Encoded': true,
+                'body': JSON.stringify({"key": "value"})
+            });
+        });
     });
 });
 
