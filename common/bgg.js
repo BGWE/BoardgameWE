5d23c3e7f4ce84d69e5cb17e714b4918afa30926
Fabrice Servais <fabrice.servais@gmail.com>
2018-05-10 00:10:44 +0200

    Clean and add more functions to parse XML and get data

--- common/bgg.js
+++ common/bgg.js
@@ -12,6 +12,14 @@ function search(boardgame_name, callback) {
     )
 }
 
+function get(boardgame_id, callback) {
+    let url_variables = {id: boardgame_id};
+    request(
+        {url: BGG_ROOT_PATH + 'thing', qs: url_variables},
+        format_get_response(callback)
+    )
+}
+
 function format_search_response(callback) {
     function _format_response(err, response, body) {
         if (err) {console.log(err); return;}
@@ -24,8 +32,8 @@ function format_search_response(callback) {
             result.items.item.forEach(function (_item) {
                 games.push({
                     'name': get_game_name_from_item(_item),
-                    'year': get_game_year_from_item(_item),
-                    'id': get_game_id_from_item(_item)
+                    'year': get_attribute(get_tag(_item, 'yearpublished')[0], 'value'),
+                    'id': get_attribute(_item, 'id')
                 })
             });
 
@@ -36,20 +44,52 @@ function format_search_response(callback) {
     return _format_response
 }
 
-function get_game_id_from_item(_json) {
-    if (_json.hasOwnProperty('$') && _json.$.hasOwnProperty('id')) return _json.$.id;
+function format_get_response(callback) {
+    function _format_response(err, response, body) {
+        if (err) {console.log(err); return;}
 
-    throw new Error('No id in ' + JSON.stringify(_json))
+        parse_string(body, function (err, result) {
+            if (err) {console.log(err); return;}
+            let game = {};
+
+            result.items.item.forEach(function (_item) {
+                console.log('====');
+                console.dir(_item);
+            });
+
+            callback(null, game);
+        });
+    }
+
+    return _format_response
 }
 
-function get_game_type_from_item(_json) {
-    if (_json.hasOwnProperty('$') && _json.$.hasOwnProperty('type')) return _json.$.type;
+function get_attribute(_json, attribute) {
+    if (_json.hasOwnProperty('$') && _json.$.hasOwnProperty(attribute)) return _json.$[attribute];
 
-    throw new Error('No type in ' + JSON.stringify(_json))
+    throw new Error('No ' + attribute + ' in ' + JSON.stringify(_json))
+}
+
+// Returns list of child tag <tag_name>
+function get_tag(_json, tag_name) {
+    if (_json.hasOwnProperty(tag_name)) return _json[tag_name];
+
+    throw new Error('No ' + tag_name + ' in ' + JSON.stringify(_json))
+}
+
+function get_attribute_from_tag(_json, tag_name, attribute) {
+    return get_attribute(get_tag(_json, tag_name)[0], attribute)
 }
 
 function get_game_name_from_item(_json) {
     if (_json.hasOwnProperty('name')) {
+        if (_json['name'].length > 1) {
+            // There could be "alternate" names, need to find the "primary" one
+            let primary_name = _json['name'].filter(function (_name) {
+                return _name.hasOwnProperty('$') && _name.$.hasOwnProperty('type') && _name.$.type === "primary"
+            });
+            return primary_name[0].$.value;
+        }
         if (_json.name[0].hasOwnProperty('$') && _json.name[0].$.hasOwnProperty('value')) {
             return _json.name[0].$.value;
         }
@@ -58,18 +98,9 @@ function get_game_name_from_item(_json) {
     throw new Error('No name in ' + JSON.stringify(_json))
 }
 
-function get_game_year_from_item(_json) {
-    if (_json.hasOwnProperty('yearpublished')) {
-        if (_json.name[0].hasOwnProperty('$') && _json.name[0].$.hasOwnProperty('value')) {
-            return _json.yearpublished[0].$.value;
-        }
-    }
-
-    throw new Error('No year in ' + JSON.stringify(_json))
-}
-
+exports.get = get;
 exports.search = search;
-exports.get_game_id_from_item = get_game_id_from_item;
-exports.get_game_type_from_item = get_game_type_from_item;
+exports.get_attribute = get_attribute;
+exports.get_tag = get_tag;
+exports.get_attribute_from_tag = get_attribute_from_tag;
 exports.get_game_name_from_item = get_game_name_from_item;
-exports.get_game_year_from_item = get_game_year_from_item;
--- test/common/bgg_test.js
+++ test/common/bgg_test.js
@@ -6,47 +6,51 @@ let _bgg = require('../../common/bgg');
 
 let json_rsp = JSON.parse('{"items":{"$":{"total":"15","termsofuse":"https://boardgamegeek.com/xmlapi/termsofuse"},"item":[{"$":{"type":"boardgame","id":"226320"},"name":[{"$":{"type":"primary","value":"My Little Scythe"}}],"yearpublished":[{"$":{"value":"2017"}}]},{"$":{"type":"boardgame","id":"169786"},"name":[{"$":{"type":"primary","value":"Scythe"}}],"yearpublished":[{"$":{"value":"2016"}}]},{"$":{"type":"boardgame","id":"199727"},"name":[{"$":{"type":"primary","value":"Scythe: Invaders from Afar"}}],"yearpublished":[{"$":{"value":"2016"}}]},{"$":{"type":"boardgame","id":"212879"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Encounter Card #37"}}],"yearpublished":[{"$":{"value":"2016"}}]},{"$":{"type":"boardgame","id":"204984"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Encounter Card #38"}}],"yearpublished":[{"$":{"value":"2016"}}]},{"$":{"type":"boardgame","id":"221033"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Encounter Card #39"}}],"yearpublished":[{"$":{"value":"2017"}}]},{"$":{"type":"boardgame","id":"232176"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Encounter Card #40"}}],"yearpublished":[{"$":{"value":"2017"}}]},{"$":{"type":"boardgame","id":"232087"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Encounter Card #41"}}],"yearpublished":[{"$":{"value":"2017"}}]},{"$":{"type":"boardgame","id":"237663"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Encounter Card #42"}}],"yearpublished":[{"$":{"value":"2017"}}]},{"$":{"type":"boardgame","id":"211731"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Pack #1"}}],"yearpublished":[{"$":{"value":"2016"}}]},{"$":{"type":"boardgame","id":"205121"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Pack #2"}}],"yearpublished":[{"$":{"value":"2016"}}]},{"$":{"type":"boardgame","id":"211732"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Pack #3"}}],"yearpublished":[{"$":{"value":"2016"}}]},{"$":{"type":"boardgame","id":"211733"},"name":[{"$":{"type":"primary","value":"Scythe: Promo Pack #4"}}],"yearpublished":[{"$":{"value":"2016"}}]},{"$":{"type":"boardgame","id":"242277"},"name":[{"$":{"type":"primary","value":"Scythe: The Rise of Fenris"}}],"yearpublished":[{"$":{"value":"2018"}}]},{"$":{"type":"boardgame","id":"223555"},"name":[{"$":{"type":"primary","value":"Scythe: The Wind Gambit"}}],"yearpublished":[{"$":{"value":"2017"}}]}]}}\n');
 let json_rsp_item = JSON.parse('{"$":{"type":"boardgame","id":"226320"},"name":[{"$":{"type":"primary","value":"My Little Scythe"}}],"yearpublished":[{"$":{"value":"2017"}}]}');
+let json_rsp_mlt_names = JSON.parse('{"$":{"type":"boardgame","id":"226320"},"name":[{"$":{"type":"primary","value":"My Little Scythe"}}, {"$":{"type":"alternative","value":"My Little Scythe2"}}],"yearpublished":[{"$":{"value":"2017"}}]}');
 let json_rsp_item_false = {'key': 'value'};
 
 describe('Getters from BGG response', function () {
-    it('should parse a JSON response and give the ID of the game.', function () {
-        let id = _bgg.get_game_id_from_item(json_rsp_item);
-
-        expect(id).to.equal("226320");
-    });
-
-    it('should parse a JSON response and give the type of the game.', function () {
-        let type = _bgg.get_game_type_from_item(json_rsp_item);
-
-        expect(type).to.equal("boardgame");
-    });
-
     it('should parse a JSON response and give the name of the game.', function () {
         let name = _bgg.get_game_name_from_item(json_rsp_item);
 
         expect(name).to.equal("My Little Scythe");
     });
 
-    it('should parse a JSON response and give the year of the game.', function () {
-        let year = _bgg.get_game_year_from_item(json_rsp_item);
+    it('should parse a JSON response and give the name of the game (multiple names).', function () {
+        let name = _bgg.get_game_name_from_item(json_rsp_mlt_names);
 
-        expect(year).to.equal("2017");
+        expect(name).to.equal("My Little Scythe");
     });
 
-    it('should parse a JSON response and give the ID of the game (false case).', function () {
-        expect(function (){_bgg.get_game_id_from_item(json_rsp_item_false)}).to.throw(Error);
+    it('should parse a JSON response and give an attribute.', function () {
+        let id = _bgg.get_attribute(json_rsp_item, 'id');
+
+        expect(id).to.equal("226320");
     });
 
-    it('should parse a JSON response and give the type of the game (false case).', function () {
-        expect(function (){_bgg.get_game_type_from_item(json_rsp_item_false)}).to.throw(Error);
+    it('should parse a JSON response and give a tag.', function () {
+        let name_tag = _bgg.get_tag(json_rsp_item, 'name');
+
+        expect(name_tag[0]).to.deep.equal({"$":{"type":"primary","value":"My Little Scythe"}});
+    });
+
+    it('should parse a JSON response and give the attribute of a tag.', function () {
+        let name = _bgg.get_attribute_from_tag(json_rsp_item, 'name', 'value');
+
+        expect(name).to.equal("My Little Scythe");
+    });
+
+
+    it('should parse a JSON response and give an attribute (false case).', function () {
+        expect(function (){_bgg.get_attribute(json_rsp_item_false, 'id')}).to.throw(Error);
+    });
+
+    it('should parse a JSON response and give a tag (false case).', function () {
+        expect(function (){_bgg.get_tag(json_rsp_item_false, 'id')}).to.throw(Error);
     });
 
     it('should parse a JSON response and give the name of the game (false case).', function () {
         expect(function (){_bgg.get_game_name_from_item(json_rsp_item_false)}).to.throw(Error);
-    });
-
-    it('should parse a JSON response and give the year of the game (false case).', function () {
-        expect(function (){_bgg.get_game_year_from_item(json_rsp_item_false)}).to.throw(Error);
     });
 });
 
@@ -58,14 +62,29 @@ describe('Search function', function () {
 
             let found_game = false;
 
-            games.forEach(function (_item) {
-                if (_item.name === "Scythe") {
-                    found_game = true;
-                    return;
-                }
+            // games.forEach(function (_item) {
+            //     if (_item.name === "Scythe") {
+            //         found_game = true;
+            //         return;
+            //     }
+            // });
+
+            found_game = games.some(function (_item) {
+                return _item.name === "Scythe";
             });
 
             assert.isTrue(found_game, "Scythe not found in response");
+            done();
+        })
+    });
+});
+
+describe('Get boardgame function', function () {
+    it('should get Boardgame Geek game from its ID.', function (done) {
+        _bgg.get("169786", function (err, game) {
+            assert(err == null, 'err is not null');
+            assert(game, 'Response is empty');
+
             done();
         })
     });
