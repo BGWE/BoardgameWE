0dd2b7b09bc4a9ac983426c163e0ced8d661fc67
Fabrice Servais <fabrice.servais@gmail.com>
2018-05-10 23:50:48 +0200

    Set Express server with root

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const http = require('http');
+let express = require('express');
 
+let app = express();
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+// const app = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World\n');
+// });
+
+app.get('/', function (req, res) {
+   res.send('Hello Ro!');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
