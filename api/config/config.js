47ec1a2e510d7c7d61b804ed799ebfe87e9eaf64
Romain Mormont <romainmormont@hotmail.com>
2018-05-11 03:44:00 +0200

    add diff. envs in config

--- api/config/config.js
+++ api/config/config.js
@@ -1,7 +1,13 @@
 
-
 module.exports = {
-    db: {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOSTNAME,
+        "dialect": "postgres"
+    },
+    production: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
