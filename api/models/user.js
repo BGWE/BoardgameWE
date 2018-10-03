83aa9a2aa20dd94212600dd95efe7fd7a0319b35
Romain Mormont <romainmormont@hotmail.com>
2018-10-04 00:11:03 +0200

    add user model

---
+++ api/models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    surname: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    username: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
