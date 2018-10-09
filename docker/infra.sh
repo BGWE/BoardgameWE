b4d1a56a218c6fda6df003861a2c3f36cdd0a481
Fabrice Servais <fabrice.servais@gmail.com>
2018-10-09 15:00:50 +0200

    Fix #11 - App is containerized to run locally

---
+++ docker/infra.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+SCRIPTDIR="$(dirname "$(which "$0")")"
+
+while test $# -gt 0
+do
+    case "$1" in
+        start) docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_backend build && docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_backend up -d --remove-orphans
+            ;;
+        stop) docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_backend stop ; docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_backend rm -f
+            ;;
+        reload) docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_backend stop ; docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_backend rm -f ; docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_backend build && docker-compose -f $SCRIPTDIR/docker-compose.yml -p bgwe_backend up -d --remove-orphans
+            ;;
+        *) echo "bad option $1"
+    esac
+    shift
+done
