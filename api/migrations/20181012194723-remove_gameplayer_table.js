fdde22803297bc4a319f0c4934980315f2fadea4
Romain Mormont <romainmormont@hotmail.com>
2018-10-12 21:50:40 +0200

    remove player and gameplayer table

---
+++ api/migrations/20181012194723-remove_gameplayer_table.js
@@ -0,0 +1,12 @@
+'use strict';
+const migration = require('./20180510210135-create-game-player.js');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return migration.down(queryInterface);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return migration.up(queryInterface);
+  }
+};
