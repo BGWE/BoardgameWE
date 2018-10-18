6d15e48f57da78db645fbf97bb9af9d9e1ff8c02
Fabrice Servais <fabrice.servais@gmail.com>
2018-10-18 11:04:09 +0200

    New staging DB

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-let cors = require('cors')
+let cors = require('cors');
 
 let app = express();
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cors());
+app.options('*', cors());
 
 app.get('/', function (req, res) {
    res.send('Hello Ro!');
