94fb8d424f68cb52f621c99ea278596b8b1326f8
Fabrice Servais <fabrice.servais@gmail.com>
2019-04-21 17:10:02 +0200

    Implements #32 - Forgot password - Send email - Remove docker-compose by a template

---
+++ api/util/email.js
@@ -0,0 +1,37 @@
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+exports.sendEmail = (toAddress, fromAddress, subject, textPayload, htmlPayload) => {
+    const msg = {
+        to: toAddress,
+        from: fromAddress,
+        subject: subject,
+        text: text,
+        html: html,
+    };
+
+    return sgMail.send(msg);
+};
+
+exports.sendResetPasswordEmail = (toAddress, fromAddress, fromName, first_name, link) => {
+    const msg = {
+        to: toAddress,
+        from: {
+            email: fromAddress,
+            name: fromName
+        },
+        subject: 'Hello world',
+        text: 'Hello plain world!',
+        html: '<p>Hello HTML world!</p>',
+        templateId: 'd-9d79c66908aa491c80db50a0f3ea4193',
+        dynamic_template_data: {
+          user: {
+              first_name: first_name
+          },
+          link: link
+        }
+    };
+    
+    return sgMail.send(msg);
+};
