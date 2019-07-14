8d39fef08c00718f1123d41488e64f93a0548e6c
Romain Mormont <romainmormont@hotmail.com>
2019-07-14 18:53:02 +0200

    implement expansion data fetching from bgg + add expansion inclusion

--- api/BoardGameController.js
+++ api/BoardGameController.js
@@ -1,9 +1,22 @@
 const db = require("./models/index");
 const bgg = require("./util/bgg");
 const util = require("./util/util");
+const lodash = require("lodash");
+const m2m = require("./util/m2m_helpers");
+const includes = require("./util/db_include");
+
+exports.boardGameIncludes = [{
+  required: false,
+  ... includes.genericIncludeSQ(db.BoardGameExpansion, 'expansions', [{
+    attributes: ['name', 'thumbnail', 'id'],
+    ... includes.genericIncludeSQ(db.BoardGame, 'expansion')
+  }])
+}];
 
 exports.getBoardGame = function(req, res) {
-    return util.sendModelOrError(res, db.BoardGame.findByPk(parseInt(req.params.bgid)));
+    return util.sendModelOrError(res, db.BoardGame.findByPk(parseInt(req.params.bgid), {
+      include: exports.boardGameIncludes
+    }));
 };
 
 exports.updateBoardGame = function(req, res) {
@@ -18,32 +31,38 @@ exports.updateBoardGame = function(req, res) {
         where: {id: bgid},
         fields: ["gameplay_video_url"]
     }).then(() => {
-        return util.sendModelOrError(res, db.BoardGame.findByPk(bgid));
+        return util.sendModelOrError(res, db.BoardGame.findByPk(bgid, {
+          include: exports.boardGameIncludes
+        }));
     }).catch(err => {
         return util.errorResponse(res);
     });
 };
 
-const boardGameFromBggResponse = function(gid, body) {
-    const game = bgg.format_get_response(body);
-    return {
-        name: game.name,
-        bgg_id: gid,
-        bgg_score: game.score,
-        gameplay_video_url: null,
-        min_players: parseInt(game.minplayers),
-        max_players: parseInt(game.maxplayers),
-        min_playing_time: parseInt(game.maxplaytime),
-        max_playing_time: parseInt(game.minplaytime),
-        playing_time: parseInt(game.playingtime),
-        thumbnail: game.thumbnail[0],
-        image: game.image[0],
-        description: game.description[0],
-        year_published: parseInt(game.yearpublished),
-        category: util.listToString(game.boardgamecategory),
-        mechanic: util.listToString(game.boardgamemechanic),
-        family: util.listToString(game.boardgamefamily)
-    };
+const formatGameFromBggResponse = function(response) {
+  return {
+    name: response.name,
+    bgg_id: response.id,
+    bgg_score: response.score,
+    gameplay_video_url: null,
+    min_players: parseInt(response.minplayers),
+    max_players: parseInt(response.maxplayers),
+    min_playing_time: parseInt(response.maxplaytime),
+    max_playing_time: parseInt(response.minplaytime),
+    playing_time: parseInt(response.playingtime),
+    thumbnail: response.thumbnail[0],
+    image: response.image[0],
+    description: response.description[0],
+    year_published: parseInt(response.yearpublished),
+    category: util.listToString(response.boardgamecategory),
+    mechanic: util.listToString(response.boardgamemechanic),
+    family: util.listToString(response.boardgamefamily)
+  };
+};
+
+const boardGameFromBggResponse = function(body) {
+    const games = bgg.format_get_response(body);
+    return games.map(formatGameFromBggResponse);
 };
 
 /**
@@ -75,11 +94,14 @@ exports.executeIfBoardGameExists = function(gid, source, req, res, actionFn) {
 };
 
 exports.addBoardGame = function(req, res) {
-    // load info from board game geek
-    const bggId = parseInt(req.body.bgg_id);
-    return bgg.getBoardGamePromise(bggId, res, body => {
-        return util.sendModelOrError(res, db.BoardGame.create(boardGameFromBggResponse(bggId, body)));
+  // load info from board game geek
+  return db.sequelize.transaction(transaction => {
+    return exports.addBoardGameAndExpensions(req.body.bgg_id, transaction).then(id => {
+      return util.sendModelOrError(res, db.BoardGame.findByPk(id, {transaction, include: exports.boardGameIncludes}));
     });
+  }).catch(err => {
+    return util.detailErrorResponse(res, 500, "cannot add board game from bgg");
+  });
 };
 
 exports.getBoardGames = function(req, res) {
@@ -102,4 +124,80 @@ exports.searchBoardGames = function(req, res) {
 exports.deleteBoardGame = function(req, res) {
     const bgid = parseInt(req.params.bgid);
     return util.handleDeletion(res, db.BoardGame.destroy({ where: {id: bgid} }));
+};
+
+/**
+ * Add a board game from bgg. Are also added to the database all board games which are either expansions of this board
+ * game, or of which this game is an expansion, or which are expansions of the board game of which this game is an
+ * expansion. The database is updated for reflecting the expansion tree.
+ * @param bgg_id Board game geek identifier
+ * @param transaction The transaction in the context of which to execute the operation
+ * @returns {Promise<int>} The bgc identifier of the board game
+ */
+exports.addBoardGameAndExpensions = async function (bgg_id, transaction) {
+  let to_fetch = new Set([bgg_id]);
+  // stores fetched bgg data and bgc models, maps bgg id with game data
+  let bg_cache = {};
+
+  const create_cache_entry = function (cache, id, model, data, parent) { cache[id] = { parent, model, data }; };
+  const cache_has_id = (cache, id) => cache[id] !== undefined;
+
+  // if game was added before expansion support was implemented, we need to check that expansions were already downloaded
+  while (to_fetch.size > 0) {
+    // fetch data from database and bgg
+    to_fetch = Array.from(to_fetch);
+    const raw_bgg_items = await bgg.get(to_fetch);
+    const bgg_games = bgg.format_get_response(raw_bgg_items);
+    const raw_bgc_games = await db.BoardGame.findAll({
+      where: {bgg_id: { [db.Op.in]: to_fetch }}, transaction,
+      lock: transaction.LOCK.SHARE
+    });
+    const bgc_games = lodash.zipObject(raw_bgc_games.map(bg => bg.bgg_id), raw_bgc_games);
+
+    // update cache
+    for (let i = 0; i < bgg_games.length; ++i) {
+      const bgg_game = bgg_games[i];
+      const id = bgg_game.id;
+      create_cache_entry(bg_cache, id, bgc_games[id] ? bgc_games[id] : null, bgg_game);
+    }
+
+    // second pass to create next to_fetch list
+    to_fetch = new Set();
+    for (let i = 0; i < bgg_games.length; ++i) {
+      const bgg_game = bgg_games[i];
+      bgg_game.expansion_of.filter(i => !cache_has_id(bg_cache, i)).forEach(i => to_fetch.add(i));
+      bgg_game.expansions.filter(i => !cache_has_id(bg_cache, i)).forEach(i => to_fetch.add(i));
+    }
+  }
+
+  // all data have been fetched, all cache entries have bgg data
+  // save board games unknown to bgc
+  const entries_to_insert = lodash.values(bg_cache).filter(e => !e.model);
+  const bg_to_insert = entries_to_insert.map(e => formatGameFromBggResponse(e.data));
+  await db.BoardGame.bulkCreate(bg_to_insert, {transaction});
+  const raw_added_bg = await db.BoardGame.findAll({
+    where: { bgg_id: { [db.Op.in]: entries_to_insert.map(e => e.data.id) } },
+    transaction
+  });
+  raw_added_bg.forEach(bg => {
+    bg_cache[bg.bgg_id].model = bg;
+  });
+
+  // minimize the number of sql requests -> bulk update
+  // update BoardGameExpension table so that registered expansions are exactly those
+  // fetched from bgg
+  let queries = lodash.valuesIn(bg_cache).map(entry => {
+    return m2m.diffAssociations({
+      model_class: db.BoardGameExpansion,
+      fixed: { field: 'id_expanded', id: entry.model.id },
+      other: {
+        field: 'id_expansion',
+        ids: entry.data.expansions.map(id => bg_cache[id].model.id)
+      },
+      options: {transaction}
+    })
+  });
+
+  await Promise.all(lodash.flatten(queries));
+  return bg_cache[bgg_id].model.id;
 };
