18ce49efa4d53814ba7ab8f4a76e9b3487aac316
Fabrice Servais <fabrice.servais@gmail.com>
2019-08-26 23:31:55 +0200

    test - print env

--- tools/bgcinfra/configs/ecosystem.config.js
+++ tools/bgcinfra/configs/ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
             ref  : 'origin/prodinfra',
             repo : 'https://github.com/BGWE/BoardgameWE.git',
             path : '/home/ec2-user/BoardgameWE',
-            'post-deploy' : 'source /home/ec2-user/BoardgameWE/source/tools/bgcinfra/configs/secret_env.sh && npm install && pm2 startOrRestart /home/ec2-user/BoardgameWE/source/tools/bgcinfra/configs/ecosystem.config.js --env production && sudo certbot certonly --debug --nginx --non-interactive --agree-tos --domains api.boardgamecomponion.com --email fabrice.servais@gmail.com'
+            'post-deploy' : 'source /home/ec2-user/BoardgameWE/source/tools/bgcinfra/configs/secret_env.sh && echo $DB_HOSTNAME && npm install && pm2 startOrRestart /home/ec2-user/BoardgameWE/source/tools/bgcinfra/configs/ecosystem.config.js --env production && sudo certbot certonly --debug --nginx --non-interactive --agree-tos --domains api.boardgamecomponion.com --email fabrice.servais@gmail.com'
         }
     }
 };
