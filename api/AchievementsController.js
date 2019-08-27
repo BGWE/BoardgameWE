75daa601a5bc3cfcad9e9c8c77ff7221374b2f3a
Romain Mormont <romainmormont@hotmail.com>
2019-08-27 15:58:19 +0200

    fix checkAchievements function

--- api/AchievementsController.js
+++ api/AchievementsController.js
@@ -1,7 +1,7 @@
 const db = require("./models/index");
 const userutil = require("./util/user");
 const util = require("./util/util");
-const achievements = require("./achievements/definition");
+const Achievements = require("./achievements/definition");
 const lodash = require("lodash");
 
 const addAchievement = function (id_user, achievement) {
@@ -15,31 +15,30 @@ exports.checkAchievements = function(id_user, scopes, check_existing) {
   if (check_existing) {
     throw new Error("not implemented error for 'checkAchievements' with 'check_existing==true'.");
   }
-  scopes = scopes || lodash.valuesIn(achievements.S);
+  scopes = scopes || lodash.valuesIn(Achievements.S);
   return db.sequelize.transaction(async transaction => {
     const current = await db.UserAchievement.findAll({ where: {id_user}, transaction, lock: transaction.LOCK.UPDATE });
-    const current_codes = new Set(current.map(a => a.code));
+    const current_codes = new Set(current.map(a => a.id_achievement));
     let new_codes = [];
-
     // only check specified scopes
-    scopes.forEach(async scope => {
-      const scoped = achievements.PER_SCOPE[scope];
-      scoped.badges.forEach(async badge => {
-        const step_codes = new Set(badge.steps().map(s => s.code));
+    await Promise.all(scopes.map(async scope => {
+      const scoped = Achievements.PER_SCOPE[scope];
+      await Promise.all(scoped.badges.map(async badge => {
+        const step_codes = new Set(badge.steps.map(s => s.code));
         if (util.set_diff(step_codes, current_codes).size > 0) { // don't check if all steps were validated
-          const steps = await badge.validated_steps(id_user, { transaction, lock: transaction.LOCK.SHARE });
-          new_codes.concat(steps.map(s => s.code));
+          const steps = await badge.validated_steps(id_user, { transaction });
+          new_codes = new_codes.concat(steps.filter(s => !current_codes.has(s.code)).map(s => s.code));
         }
-      });
-      scoped.achievements.forEach(async achievement => {
+      }));
+      await Promise.all(scoped.achievements.map(async achievement => {
         if (!current_codes.has(achievement.code)) { // only check if has not been validated
-          const validated = await achievement.check(id_user, { transaction, lock: transaction.LOCK.SHARE });
+          const validated = await achievement.check(id_user, { transaction });
           if (validated) {
-            new_codes.concat(achievement.code);
+            new_codes.push(achievement.code);
           }
         }
-      });
-    });
+      }));
+    }));
 
     const new_entries = new_codes.map(c => { return {id_achievement: c, id_user}; });
     return await db.UserAchievement.bulkCreate(new_entries, { transaction });
@@ -50,7 +49,7 @@ exports.sendUserAchievements = function (req, res, id_user) {
   return db.UserAchievement.findAll({
     where: {id_user}
   }).then(achvmts => {
-    return util.successResponse(res, achievements.format_all(achvmts, req));
+    return util.successResponse(res, Achievements.format_all(achvmts, req));
   }).catch(err => {
     return util.errorResponse(res);
   });
@@ -75,8 +74,8 @@ exports.getUserAchievements = function(req, res) {
 };
 
 exports.addOnionAchievement = function(req, res) {
-  return addAchievement(userutil.getCurrUserId(req), achievements.A.EASTER_EGG_ONION).then(achvmt => {
-    return util.successResponse(res, achievements.format(achvmt, req));
+  return addAchievement(userutil.getCurrUserId(req), Achievements.A.EASTER_EGG_ONION).then(achvmt => {
+    return util.successResponse(res, Achievements.format(achvmt, req));
   }).catch(err => {
     return util.errorResponse(res);
   });
