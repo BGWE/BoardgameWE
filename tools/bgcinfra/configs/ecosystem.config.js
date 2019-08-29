9f1435b76de8c8ae34e23fa5d56972cdeb6a1d8e
Fabrice Servais <fabrice.servais@gmail.com>
2019-08-29 22:08:39 +0200

    Development environment

--- tools/bgcinfra/configs/ecosystem.config.js
+++ tools/bgcinfra/configs/ecosystem.config.js
@@ -11,7 +11,11 @@ module.exports = {
         watch: false,
         max_memory_restart: '1G',
         env: {
-            NODE_ENV: 'development'
+            NODE_ENV: 'development',
+            DB_HOSTNAME: 'ec2-46-137-188-105.eu-west-1.compute.amazonaws.com',
+            DB_NAME: 'd283kb5i1s2nda',
+            DB_USERNAME: 'negjhbtiwkpkiv',
+            USE_SSL: 'true',
         },
         env_production: {
             NODE_ENV: 'production',
@@ -23,6 +27,20 @@ module.exports = {
     }],
     
     deploy : {
+        development: {
+            user: 'ec2-user',
+            host : 'apiredirect.boardgamecomponion.com',
+            ref  : 'origin/develop',
+            repo : 'https://github.com/BGWE/BoardgameWE.git',
+            path : '/home/ec2-user/BoardgameWE',
+            'post-deploy' : '\
+                npm install && \
+                cp /home/ec2-user/BoardgameWE/source/tools/bgcinfra/configs/develop_env.sh /home/ec2-user/BoardgameWE/source/.env && \
+                pm2 start npm -- run envstart && \
+                sudo certbot certonly --debug --nginx --non-interactive --agree-tos --domains api-dev.boardgamecomponion.com --email fabrice.servais@gmail.com && \
+                sudo cp /tmp/https.conf /etc/nginx/conf.d/https.conf && \
+                sudo nginx -s reload'
+        },
         production : {
             user: 'ec2-user',
             host : 'apiredirect.boardgamecomponion.com',
