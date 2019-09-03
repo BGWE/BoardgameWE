198d5a2798219cc723fbabc39a03de5bb672dde7
Fabrice Servais <fabrice.servais@gmail.com>
2019-09-03 15:54:57 +0200

    Centralize deploy commands

--- tools/bgcinfra/configs/deploy.sh
+++ tools/bgcinfra/configs/deploy.sh
@@ -3,16 +3,16 @@
 # any future command that fails will exit the script
 set -e
 
-# Lets write the public key of our aws instance
-eval $(ssh-agent -s)
-echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
-# pwd
-mkdir -p ~/.ssh
-# echo -e "$SSH_KEY" > ~/.ssh/id_rsa
-# chmod 600 ~/.ssh/id_rsa
-echo -e "$SSH_KEY" > ~/.ssh/BGCDev.pem
-chmod 600 ~/.ssh/BGCDev.pem
-cat ~/.ssh/BGCDev.pem
+echo $TRAVIS_BRANCH
+
+openssl aes-256-cbc -K $encrypted_3f4d3615b6b0_key -iv $encrypted_3f4d3615b6b0_iv -in BGCDev.pem.enc -out /tmp/BGCDev.pem -d
+eval "$(ssh-agent -s)"
+chmod 600 /tmp/BGCDev.pem
+ssh-add /tmp/BGCDev.pem
+echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 # disable the host key checking.
 bash ./tools/bgcinfra/configs/disableHostKeyChecking.sh
+
+# Deploy
+pm2 deploy ./tools/bgcinfra/configs/ecosystem.config.js development
