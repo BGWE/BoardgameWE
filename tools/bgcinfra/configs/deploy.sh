9b4264b04a29ee5a2f4bee5a509bdbdaa2ae57a3
Fabrice Servais <fabrice.servais@gmail.com>
2019-09-03 16:10:54 +0200

    Travis - Debug

--- tools/bgcinfra/configs/deploy.sh
+++ tools/bgcinfra/configs/deploy.sh
@@ -4,6 +4,9 @@
 set -e
 
 addSSHKey () {
+    echo "$1"
+    echo "$2"
+    echo "$3"
     openssl aes-256-cbc -K $1 -iv $2 -in $3 -out /tmp/key.pem -d
     eval "$(ssh-agent -s)"
     chmod 600 /tmp/key.pem
@@ -12,7 +15,7 @@ addSSHKey () {
 
 if [[ $TRAVIS_BRANCH == "develop" ]]; then
     echo "Deploy for branch 'develop'"
-    addSSHKey "$encrypted_3f4d3615b6b0_key" "$encrypted_3f4d3615b6b0_iv" "BGCDev.pem.enc"
+    addSSHKey $encrypted_3f4d3615b6b0_key $encrypted_3f4d3615b6b0_iv "BGCDev.pem.enc"
 
     # disable the host key checking.
     bash ./tools/bgcinfra/configs/disableHostKeyChecking.sh
