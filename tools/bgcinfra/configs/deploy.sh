0322a71c35c3b432c95b026a69941cc2e3a7e055
Fabrice Servais <fabrice.servais@gmail.com>
2019-09-03 13:39:34 +0200

    Debug - create .ssh dir

--- tools/bgcinfra/configs/deploy.sh
+++ tools/bgcinfra/configs/deploy.sh
@@ -7,7 +7,7 @@ set -e
 eval $(ssh-agent -s)
 echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
 # pwd
-# mkdir -p ~/.ssh
+mkdir -p ~/.ssh
 # echo -e "$SSH_KEY" > ~/.ssh/id_rsa
 # chmod 600 ~/.ssh/id_rsa
 echo -e "$SSH_KEY" > ~/.ssh/BGCDev.pem
