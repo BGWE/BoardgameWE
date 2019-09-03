8c60bcb01044d446bd3d1fc756b6ae5ebd2f4c31
Fabrice Servais <fabrice.servais@gmail.com>
2019-09-03 13:37:03 +0200

    Debug - add pem file

--- tools/bgcinfra/configs/deploy.sh
+++ tools/bgcinfra/configs/deploy.sh
@@ -10,8 +10,9 @@ echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
 # mkdir -p ~/.ssh
 # echo -e "$SSH_KEY" > ~/.ssh/id_rsa
 # chmod 600 ~/.ssh/id_rsa
-# echo -e "$SSH_KEY" > ~/.ssh/BGCDev.pem
-# chmod 600 ~/.ssh/BGCDev.pem
+echo -e "$SSH_KEY" > ~/.ssh/BGCDev.pem
+chmod 600 ~/.ssh/BGCDev.pem
+cat ~/.ssh/BGCDev.pem
 
 # disable the host key checking.
 bash ./tools/bgcinfra/configs/disableHostKeyChecking.sh
