583ddb229bcdbb0ec6dd6e301fee676cdf655bb7
Fabrice Servais <fabrice.servais@gmail.com>
2019-09-03 13:24:02 +0200

    Retry with ssh-agent method

--- tools/bgcinfra/configs/deploy.sh
+++ tools/bgcinfra/configs/deploy.sh
@@ -4,14 +4,14 @@
 set -e
 
 # Lets write the public key of our aws instance
-# eval $(ssh-agent -s)
-# echo "$" | tr -d '\r' | ssh-add - > /dev/null
-pwd
-mkdir -p ~/.ssh
-echo -e "$SSH_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-echo -e "$SSH_KEY" > ~/.ssh/BGCDev.pem
-chmod 600 ~/.ssh/BGCDev.pem
+eval $(ssh-agent -s)
+echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
+# pwd
+# mkdir -p ~/.ssh
+# echo -e "$SSH_KEY" > ~/.ssh/id_rsa
+# chmod 600 ~/.ssh/id_rsa
+# echo -e "$SSH_KEY" > ~/.ssh/BGCDev.pem
+# chmod 600 ~/.ssh/BGCDev.pem
 
 # disable the host key checking.
 bash ./tools/bgcinfra/configs/disableHostKeyChecking.sh
