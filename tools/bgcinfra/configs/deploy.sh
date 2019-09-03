a0df94eb56cb9050d15eccc63b42dbae7039c4db
Fabrice Servais <fabrice.servais@gmail.com>
2019-09-03 16:02:06 +0200

    Deploy if develop branch

--- tools/bgcinfra/configs/deploy.sh
+++ tools/bgcinfra/configs/deploy.sh
@@ -3,16 +3,18 @@
 # any future command that fails will exit the script
 set -e
 
-echo $TRAVIS_BRANCH
+if [[ $TRAVIS_BRANCH == "develop" ]]; then
+    echo "Deploy for branch 'develop'"
+    openssl aes-256-cbc -K $encrypted_3f4d3615b6b0_key -iv $encrypted_3f4d3615b6b0_iv -in BGCDev.pem.enc -out /tmp/BGCDev.pem -d
+    eval "$(ssh-agent -s)"
+    chmod 600 /tmp/BGCDev.pem
+    ssh-add /tmp/BGCDev.pem
 
-openssl aes-256-cbc -K $encrypted_3f4d3615b6b0_key -iv $encrypted_3f4d3615b6b0_iv -in BGCDev.pem.enc -out /tmp/BGCDev.pem -d
-eval "$(ssh-agent -s)"
-chmod 600 /tmp/BGCDev.pem
-ssh-add /tmp/BGCDev.pem
-echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+    # disable the host key checking.
+    bash ./tools/bgcinfra/configs/disableHostKeyChecking.sh
 
-# disable the host key checking.
-bash ./tools/bgcinfra/configs/disableHostKeyChecking.sh
-
-# Deploy
-pm2 deploy ./tools/bgcinfra/configs/ecosystem.config.js development
+    # Deploy
+    pm2 deploy ./tools/bgcinfra/configs/ecosystem.config.js development
+else
+    echo "Current branch: '$TRAVIS_BRANCH', skipping deployment..."
+fi
