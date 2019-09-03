31b51a1445a5bbc0741e5ac89136742233298f55
Fabrice Servais <fabrice.servais@gmail.com>
2019-09-03 11:26:09 +0200

    Try alternative approach for SSH private key

--- tools/bgcinfra/configs/deploy.sh
+++ tools/bgcinfra/configs/deploy.sh
@@ -4,8 +4,11 @@
 set -e
 
 # Lets write the public key of our aws instance
-eval $(ssh-agent -s)
-echo "$SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
+# eval $(ssh-agent -s)
+# echo "$" | tr -d '\r' | ssh-add - > /dev/null
+pwd
+echo -e "$SSH_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 
 # disable the host key checking.
 ./disableHostKeyChecking.sh
