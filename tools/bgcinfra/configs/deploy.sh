8cac8fa6bec099cf720fb2c1258b6dc012feb434
Fabrice Servais <fabrice.servais@gmail.com>
2019-09-03 16:17:39 +0200

    Deploy in prod + simplifying

--- tools/bgcinfra/configs/deploy.sh
+++ tools/bgcinfra/configs/deploy.sh
@@ -4,34 +4,27 @@
 set -e
 
 addSSHKey () {
-    echo "$1"
-    echo "$2"
-    echo "$3"
-    openssl aes-256-cbc -K $1 -iv $2 -in $3 -out /tmp/key.pem -d
+    openssl aes-256-cbc -K $encrypted_3f4d3615b6b0_key -iv $encrypted_3f4d3615b6b0_iv -in $1 -out /tmp/key.pem -d
     eval "$(ssh-agent -s)"
     chmod 600 /tmp/key.pem
     ssh-add /tmp/key.pem
 }
 
+# disable the host key checking.
+bash ./tools/bgcinfra/configs/disableHostKeyChecking.sh
+
 if [[ $TRAVIS_BRANCH == "develop" ]]; then
     echo "Deploy for branch 'develop'"
-    addSSHKey $encrypted_3f4d3615b6b0_key $encrypted_3f4d3615b6b0_iv "BGCDev.pem.enc"
 
-    # disable the host key checking.
-    bash ./tools/bgcinfra/configs/disableHostKeyChecking.sh
+    addSSHKey "BGCDev.pem.enc"
 
     # Deploy
     pm2 deploy ./tools/bgcinfra/configs/ecosystem.config.js development
 
 elif [[ $TRAVIS_BRANCH == "master" ]]; then
-    echo "Deploy for branch 'develop'"
-    openssl aes-256-cbc -K $encrypted_3f4d3615b6b0_key -iv $encrypted_3f4d3615b6b0_iv -in BGCProd.pem.enc -out /tmp/BGCDev.pem -d
-    eval "$(ssh-agent -s)"
-    chmod 600 /tmp/BGCDev.pem
-    ssh-add /tmp/BGCDev.pem
+    echo "Deploy for branch 'master'"
 
-    # disable the host key checking.
-    bash ./tools/bgcinfra/configs/disableHostKeyChecking.sh
+    addSSHKey "BGCProd.pem.enc"
 
     # Deploy
     pm2 deploy ./tools/bgcinfra/configs/ecosystem.config.js production
