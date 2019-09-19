2f0326524998deb29ab3b5dcf2cf0ae49e7f5b26
Waliens <r.mormont@uliege.be>
2019-09-19 16:01:55 +0200

    change indent

--- websockets/sockets.js
+++ websockets/sockets.js
@@ -3,23 +3,22 @@ const config = require("../api/config/config.js");
 const timers = require("./timers");
 const util = require("./util/util");
 
-module.exports = function(io) {
-    // timer namespace
-    // vue-socket.io-extended does not support namespaces
-    // const timerNamespace = io.of("/timer");
+module.exports = function (io) {
+  // timer namespace
+  // vue-socket.io-extended does not support namespaces
+  // const timerNamespace = io.of("/timer");
 
-    // user must be authenticated to use this namespace
-    io.on('connection', socketioJwt.authorize({
-        secret: config.jwt_secret_key
-    })).on('authenticated', (auth_sckt) => {
-        auth_sckt.logger = io.logger;
+  // user must be authenticated to use this namespace
+  io.on('connection', socketioJwt.authorize({
+    secret: config.jwt_secret_key
+  })).on('authenticated', (auth_sckt) => {
+    auth_sckt.logger = io.logger;
 
-        let disconnect_hndlers = [];
-        disconnect_hndlers.push(timers.attachHandlers(io, auth_sckt));
+    let disconnect_hndlers = [];
+    disconnect_hndlers.push(timers.attachHandlers(io, auth_sckt));
 
-        util.on(auth_sckt, "disconnect", async function() {
-          disconnect_hndlers.forEach(async fn => await fn(auth_sckt, io));
-        });
+    util.on(auth_sckt, "disconnect", async function () {
+      disconnect_hndlers.forEach(async fn => await fn(auth_sckt, io));
     });
-
+  });
 };
