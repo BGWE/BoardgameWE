b8816e8d4b59999e65f15819af1639476b23b3f4
hjwk <woszczykh@gmail.com>
2019-10-04 12:41:14 +0200

    housekeeping

--- api/GameController.js
+++ api/GameController.js
@@ -1,7 +1,6 @@
 const db = require("./models/index");
 const util = require("./util/util");
 const includes = require("./util/db_include");
-const userutil = require("./util/user");
 
 exports.gameFullIncludesSQ = [
     includes.defaultBoardGameIncludeSQ,
@@ -117,7 +116,7 @@ exports.addGame = function (req, res) {
 
 exports.updateGameQuery = function(gid, req, res) {
   return db.sequelize.transaction(async t => {
-    let game = await db.Game.findByPk(req.params.gid, {transaction: t});
+    let game = await db.Game.findByPk(gid, {transaction: t});
     if (!game) {
       return util.detailErrorResponse(res, 404, "game not found");
     }
