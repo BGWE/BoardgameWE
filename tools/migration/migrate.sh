a44000c83aa1f32b8b36318a35000b46630e826b
Fabrice Servais <fabrice.servais@gmail.com>
2020-05-03 21:04:17 +0200

    Fix migrate

--- tools/migration/migrate.sh
+++ tools/migration/migrate.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-. .env
-cat .env
-echo $DB_HOSTNAME
-npx apidoc -i src/api/ -o -i src/doc/; sleep 5; npx sequelize db:migrate
+. .env && npx apidoc -i src/api/ -o -i src/doc/; sleep 5; npx sequelize db:migrate
