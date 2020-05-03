6026123f1ad7696054de1a11802ca2defba1c080
Fabrice Servais <fabrice.servais@gmail.com>
2020-05-04 00:35:40 +0200

    Remove export .env

--- tools/bgcinfra/configs/secret_env.sh
+++ tools/bgcinfra/configs/secret_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export CERTDOMAIN=api-v3.boardgamecomponion.com
-export NODE_ENV=production
-export DB_HOSTNAME=bgc-db-instance.cbzmmlktqvhn.eu-west-1.rds.amazonaws.com
-export DB_NAME=bgcomponion
-export DB_USERNAME=dbadmin
-export USE_SSL=true
-export NODE_TLS_REJECT_UNAUTHORIZED=0
-export DB_PASSWORD=$(aws --region eu-west-1 ssm get-parameter --name DB_PASSWORD --query 'Parameter.Value' --output text)
-export JWT_SECRET_KEY=$(aws --region eu-west-1 ssm get-parameter --name JWT_SECRET_KEY --query 'Parameter.Value' --output text)
-export SENDGRID_API_KEY=$(aws --region eu-west-1 ssm get-parameter --name SENDGRID_API_KEY --query 'Parameter.Value' --output text)
+CERTDOMAIN=api-v3.boardgamecomponion.com
+NODE_ENV=production
+DB_HOSTNAME=bgc-db-instance.cbzmmlktqvhn.eu-west-1.rds.amazonaws.com
+DB_NAME=bgcomponion
+DB_USERNAME=dbadmin
+USE_SSL=true
+NODE_TLS_REJECT_UNAUTHORIZED=0
+DB_PASSWORD=$(aws --region eu-west-1 ssm get-parameter --name DB_PASSWORD --query 'Parameter.Value' --output text)
+JWT_SECRET_KEY=$(aws --region eu-west-1 ssm get-parameter --name JWT_SECRET_KEY --query 'Parameter.Value' --output text)
+SENDGRID_API_KEY=$(aws --region eu-west-1 ssm get-parameter --name SENDGRID_API_KEY --query 'Parameter.Value' --output text)
